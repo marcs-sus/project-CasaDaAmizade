d6883637b8adef70088b170a849c075789bb470f
Lucas G. Stopassolli <lucasgstopassolli@gmail.com>
2024-10-17 20:15:03 -0300

    navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,8 +1,21 @@
-<nav class="navbar">
-    <a href="index.php">Home</a>
-    <a href="about.php">Quem Somos</a>
-    <a href="activities.php">Atividade></a>
-    <a href="transparency.php">Transparência</a>
-    <a href="contact.php">Fale Conosco</a>
-    <a href="donations.php">Doe Agora!</a>
-</nav>
+<header>
+    <div class="container">
+        <div class="idv-site">
+            <a href="#"><img src="img/logo.png" alt="logo" class="logo"></a>
+        </div>
+        <nav class="menu-upper">
+            <a href="#"><button class="btn">Quem Somos</button></a> 
+            <a href="#"><button class="btn">Atividade</button></a>
+            <div class="dropdown">
+            <a href="#"><button class="dropbtn">Transparência</button></a>
+                <div class="dropdown-content">
+                    <a href="#">Ética</a>
+                </div>
+            </div>
+            <a href="#"><button class="btn">Fale Conosco</button></a>
+        </nav>
+        <div class="but-contact">
+            <a href="#"><button class="donation" href>Doe Agora!</button></a> 
+        </div>
+    </div>
+</header>

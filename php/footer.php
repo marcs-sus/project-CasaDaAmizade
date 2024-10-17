49940ae29325b7843cabe1e0dcf008703703625d
Fives2 <gamersbr.bolo@gmail.com>
2024-10-17 19:30:52 -0300

    footer 3.2

--- php/footer.php
+++ php/footer.php
@@ -28,19 +28,18 @@
                 <input class="btn btn-outline-light" type="submit" value="Enviar">
             </form>
         </div>
-</footer>
 
 <div class="separa"></div>
 
 <div class="detalhes-contato">
     <h5>Contatos</h5>
-    <p><strong>Telefone:</strong> (47) 99938-3195</p>
-    <p><strong>Email:</strong> casaamizade@gmail.com</p>
+    <p><img src="img/telefone.png" alt="" style="width: 3vh;"><strong>Telefone:</strong> (47) 99938-3195</p>
+    <p><img src="img/email.png" alt="" style="width: 3vh;"><strong>Email:</strong> casaamizade@gmail.com</p>
 </div>
 
 <div class="localizacao">
     <h5>Localização</h5>
-    <p>Lontras, Centro</p>
+    <p><img src="img/local.png" alt="" style="width: 3vh;">Lontras, Centro</p>
 </div>
 
 <div class="redes-sociais">
@@ -54,6 +53,8 @@
 <div class="rodape-inferior">
     <p>Todos os Direitos Reservados Casa da Amizade © 2024</p>
 </div>
+
+</footer>
 
 <!-- Sistema mensagem anônima -->
 <script>

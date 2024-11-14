337989867e171af56dfb7120727c02b497ef2ac2
AnaPaulaFertigBastos <anapfb25@gmail.com>
2024-11-14 19:10:39 -0300

    arrumando letra rafael

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Casa da Amizade</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+  <!-- Additional Stylesheets -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<body>
+  <!-- Header section -->
+  <header>
+    <h1>Casa da Amizade</h1>
+  </header>
+
+  <!-- Main section -->
+  <main>
+    <!-- Navigation section -->
+    <?php include "php/navbar.php" ?>
+
+    <!-- Banner/Hero section -->
+    <section id="hero">
+      <div class="principal_imagem">
+        <img src="#" alt="Casa da Amizade">
+      </div>
+    </section>
+
+    <!-- News Section -->
+    <section class="principal_noticias" id="news">
+      <h3 class="principal_noticias_titulo">Notícias</h3>
+      <div class="principal_noticias_grid">
+        <div class="principal_noticias_grid_grande">
+          <!-- placeholder for future code -->
+        </div>
+      </div>
+    </section>
+
+    <!-- Intro section -->
+    <section id="intro">
+      <!-- intro code here -->
+    </section>
+  </main>
+
+  <!-- Footer section -->
+  <?php include 'php/footer.php'; ?>
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
+  <!-- Popper.js -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+
+  <!-- Additional Scripts -->
+  <script src="js/script.js"></script>
+  <script src="/path/to/conditional-fields.umd.js"></script>
+</body>
+
+</html>
---
+++ pages/donations.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casa da Amizade - Doações</title>
+</head>
+
+<body>
+    <!-- Header section -->
+    <header>
+        <h1>Doe Agora!</h1>
+    </header>
+
+    <!-- Main section -->
+    <main>
+        <!-- Navigation section -->
+        <?php include "php/navbar.php" ?>
+
+        <!-- QR code section -->
+        <section id="qr-code">
+            <img src="" alt="QR code">
+        </section>
+    </main>
+</body>
+
+</html>
